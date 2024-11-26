153f27f4f42401082f7b9ea73a83e0fe434ec746
Jeahyuk(Shawn) Jeong <44784961+wjdwpgur23@users.noreply.github.com>
2024-11-26 21:49:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_project_name",
+    name="CICD test",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Jeahyuk Jeong",
+    author_email="jeong_j1@denison.edu",
     description="A short description of your project",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/wjdwpgur23/banthig/CICD_test",
+    url="https://github.com/ban-thing/CICD_test.git",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.9",
