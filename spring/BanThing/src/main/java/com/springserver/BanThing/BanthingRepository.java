28479db222173512cf154485c317a9cc779f6909
Jeahyuk(Shawn) Jeong <44784961+wjdwpgur23@users.noreply.github.com>
2024-11-28 13:39:23 +0900

    Delete spring directory

--- spring/BanThing/src/main/java/com/springserver/BanThing/BanthingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springserver.BanThing;
-
-import com.springserver.BanThing.banthingtest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BanthingRepository extends JpaRepository<banthingtest, Long> {
-}
--- spring/BanThing/src/main/java/com/springserver/BanThing/Config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package com.springserver.BanThing.Config;
-
-public class JwtConstant {
-    public static final String JWT_SECRET = "gawkja rbkbalnlbawrn a";
-    public static final String JWT_HEADER = "Authorization";
-}
