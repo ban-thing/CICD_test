cf89e00df409ed8787dd4f44008dcb58ed8b1a78
Jeahyuk(Shawn) Jeong <44784961+wjdwpgur23@users.noreply.github.com>
2024-11-28 14:07:35 +0900

    Add files via upload

---
+++ src/main/java/com/example/banthing/BanThingApplication.java
@@ -0,0 +1,15 @@
+package com.example.banthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BanThingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanThingApplication.class, args);
+    }
+
+}
